47a3409bb8aaa23cc1086fb8fb537a6e1081e9ac
zyntaxmods <zyntaxinc@gmail.com>
2024-12-01 19:13:12 +0800

    Update main.js

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ var no = document.getElementById("no").addEventListener('click', function(){
     yes.style.fontSize = "35px";
     btn.textContent = "Pweaseee!";
     kenshie.src = "/gifs/cat1.gif";
+    yes.style.transition = "1s";
+    btn.style.transition = "1s";
   }
   else if(count == 2){
     btn.style.padding = "20px 60px";
@@ -18,38 +20,48 @@ var no = document.getElementById("no").addEventListener('click', function(){
     yes.style.fontSize = "40px";
     btn.textContent = "C'mooon!";
     kenshie.src = "/gifs/cat2.gif";
+    yes.style.transition = "1s";
+    btn.style.transition = "1s";
   }
   else if(count == 3){
-    btn.style.padding = "15px 55px";
+    btn.style.padding = "15px 40px";
     yes.style.padding = "45px 85px"
     yes.style.fontSize = "45px";
     yes.style.boxShadow = "0px 2px 10px red"
     btn.textContent = "Plssss!";
     kenshie.src = "/gifs/cat3.gif";
+    yes.style.transition = "1s";
+    btn.style.transition = "1s";
   }
   else if(count == 4){
-    btn.style.padding = "10px 50px";
+    btn.style.padding = "10px 35px";
     yes.style.padding = "50px 90px"
     yes.style.fontSize = "50px";
     yes.style.boxShadow = "0px 5px 20px red"
     btn.textContent = "Be fr!";
     kenshie.src = "/gifs/cat4.gif";
+    yes.style.transition = "1s";
+    btn.style.transition = "1s";
   }
   else if(count == 5){
-    btn.style.padding = "5px 45px";
+    btn.style.padding = "5px 30px";
     yes.style.padding = "55px 95px"
     yes.style.fontSize = "55px";
     yes.style.boxShadow = "0px 5px 60px red"
     btn.textContent = "Rethink?";
     kenshie.src = "/gifs/cat5.gif";
+    yes.style.transition = "1s";
+    btn.style.transition = "1s";
   }
   else if(count == 6){
-    btn.style.padding = "0px 40px";
+    btn.style.padding = "0px 25px";
     yes.style.padding = "60px 100px"
     yes.style.fontSize = "50px";
     yes.style.boxShadow = "0px 5px 100px red"
     btn.textContent = "Pretty please?";
     kenshie.src = "/gifs/cat6.gif";
+    yes.style.transition = "1s";
+    btn.style.transition = "1s";
   }
   else if(count == 7){
     btn.style.display = "none";
@@ -57,6 +69,8 @@ var no = document.getElementById("no").addEventListener('click', function(){
     yes.style.fontSize = "60px";
     yes.style.boxShadow = "0px 5px 120px red"
     yes.style.textShadow = "0px 5px 120px green"
+    yes.style.transition = "1s";
+    btn.style.transition = "1s";
   }
 })
 let yes = document.getElementById("yes").addEventListener('click', function(){
